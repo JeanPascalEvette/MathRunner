f19ab495808b210be88882a84eb36f36139c0446
Jeyp <jp.evette1@gmail.com>
2015-12-11 20:25:53 +0000

    Better version of the mandelbrot - still not working

--- octet/shaders/mandelbrot.fs
+++ octet/shaders/mandelbrot.fs
@@ -8,6 +8,13 @@
 varying vec3 model_pos_;
 uniform float width;
 uniform float height;
+		
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
 
 void main() { 
 
@@ -21,20 +28,19 @@ void main() {
     float zoom = 1.0;
 	float moveX = 0.0;
 	float moveY = 0.0; //you can change these to zoom and change position
-    vec3 color; //the RGB color value for the pixel
     int maxIterations = 300; //after how much iterations the function should stop
 
     //pick some values for the constant c, this determines the shape of the Julia Set
     cRe = -0.7;
     cIm = 0.27015;
 
-	float x = model_pos_.x + width/2;
-	float y = model_pos_.y + height/2;
+	float x = model_pos_.x;
+	float y = model_pos_.y;
 
 
 	//calculate the initial real and imaginary part of z, based on the pixel location and zoom and position values
-	newRe = 1.5 * (x - w / 2) / (0.5 * zoom * w) + moveX;
-	newIm = (y - h / 2) / (0.5 * zoom * h) + moveY;
+	newRe = 1.5 * (x - width / 2.0) / (0.5 * zoom * width) + moveX;
+	newIm = (y - height / 2.0) / (0.5 * zoom * height) + moveY;
 	//i will represent the number of iterations
 	int i;
 	//start the iteration process
@@ -45,20 +51,20 @@ void main() {
 		oldIm = newIm;
 		//the actual iteration, the real and imaginary part are calculated
 		newRe = oldRe * oldRe - oldIm * oldIm + cRe;
-		newIm = 2 * oldRe * oldIm + cIm;
+		newIm = 2.0 * oldRe * oldIm + cIm;
 		//if the point is outside the circle with radius 2: stop
-		if((newRe * newRe + newIm * newIm) > 4) break;
+		if((newRe * newRe + newIm * newIm) > 4.0) break;
 	}
 	//use color model conversion to get rainbow palette, make brightness black if maxIterations reached
-	color = hsv2rgb(vec3(i % 256, 255, 255 * (i < maxIterations)));
+	float colorH = mod(float(i),256.0);
+	float colorS = 255.0;
+	float colorV = 255.0;
+	if(i > maxIterations)
+	colorV *= 0.0;
+	vec3 colorHSV = vec3(colorH, colorS, colorV);
+	vec3 colorRGB = hsv2rgb(colorHSV);
 	//draw the pixel
-	gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
+	gl_FragColor = vec4(colorRGB, 1.0);
 
 }
-			
-vec3 hsv2rgb(vec3 c)
-{
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
+	
